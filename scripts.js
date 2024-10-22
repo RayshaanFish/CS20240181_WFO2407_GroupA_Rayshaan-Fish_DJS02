16fcc0d4dd7caa9c780007dc7cd6562c71f79097
Rayshaan Fish <Rayshaan94@gmail.com>
2024-10-22 15:41:22 +0200

    DJS02 complete, that was simple

--- scripts.js
+++ scripts.js
@@ -5,5 +5,37 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  // convert inputs to numbers
+  const isNumberDividend = Number(dividend);
+  const isNumberDivider = Number(divider);
+
+  // Empty inputs
+  if (dividend === "" || divider === "") {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+
+  // Divide by 0 Error
+  if (isNumberDivider === 0) {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again";
+
+    // Log the error to the callstack
+    console.error(new Error("divided by zero - they must be stupid!!"));
+    return;
+  }
+
+  if (isNaN(isNumberDividend) || isNaN(isNumberDivider)) {
+    document.body.innerHTML =
+      "Something critical went wrong. Please reload the page";
+    console.error(new Error("User entered 'non-numbers'"));
+    return;
+  }
+
+  // Round down the result
+  const resultOfDivision = Math.floor(dividend / divider);
+
+  result.innerText = resultOfDivision;
 });
